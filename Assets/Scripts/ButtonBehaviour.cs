50204169da4ac6dc6981e5d1ed81f2e4f54dcfc9
triruz <101410794+triruz@users.noreply.github.com>
2022-05-06 14:25:55 -0300

    6/5 - Buttons/while

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+
+
+    public void showMessageConsole()
+    {
+        Debug.Log("Hola Mundo");
+    }
+}
---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+
+    public GameObject objectToClone;
+
+    public void defede()
+    {
+        int counter = 0;
+        while (counter < 3)
+        {
+            Instantiate(objectToClone);
+            counter++;
+        }
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    int counter = 0;
+    //    while (counter<3) {
+    //        Instantiate(objectToClone);
+    //        counter++;
+    //    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
